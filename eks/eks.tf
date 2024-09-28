f93207d44502105d4e86a5ef6bba0232e9cc4b85
ederpbrito <ederpsilva@hotmail.com>
2024-09-27 21:51:03 -0300

    refactoração codigo

--- eks/eks.tf
+++ eks/eks.tf
@@ -1,16 +1,16 @@
-### EKS subnets
-# Referencing public subnets
-# data "aws_subnets" "public-subnets" {
-#   filter {
-#     name   = "vpc-id"
-#     values = [data.aws_vpc.techchallenge-vpc.id]
-#   }
+## EKS subnets
+#Referencing public subnets
+data "aws_subnets" "public-subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.techchallenge-vpc.id]
+  }
 
-#   filter {
-#     name   = "tag:Name"
-#     values = ["*public*"]
-#   }
-# }
+  filter {
+    name   = "tag:Name"
+    values = ["*public*"]
+  }
+}
 
 
 resource "aws_eks_cluster" "techchallenge_eks_cluster" {
@@ -20,7 +20,7 @@ resource "aws_eks_cluster" "techchallenge_eks_cluster" {
 
   vpc_config {
     security_group_ids = [aws_security_group.eks_security_group.id]
-    subnet_ids         = aws_subnet.eks_subnet[*].id
+    subnet_ids         = data.aws_subnets.private-subnets.ids
   }
 }
 
@@ -28,36 +28,36 @@ resource "aws_eks_fargate_profile" "fargate_profile" {
   cluster_name           = "techchallenge-eks-cluster"
   fargate_profile_name   = "fargate-profile"
   pod_execution_role_arn = "arn:aws:iam::117590171476:role/LabRole"
-  subnet_ids             = aws_subnet.eks_subnet[*].id
+  subnet_ids             = data.aws_subnets.private-subnets.ids
 
   selector {
     namespace = "default"
   }
 
-  depends_on = [aws_eks_cluster.techchallenge_eks_cluster]
+  depends_on = [data.aws_vpc.techchallenge-vpc]
 
 }
 
 resource "aws_security_group" "eks_security_group" {
   name        = "eks-security-group"
   description = "Controla o acesso ao cluster EKS"
-  vpc_id      = aws_vpc.eks_vpc.id
+  vpc_id      = data.aws_vpc.techchallenge-vpc.id
 }
 
-resource "aws_vpc" "eks_vpc" {
-  cidr_block = "10.0.0.0/16"
-}
+# resource "aws_vpc" "eks_vpc" {
+#   cidr_block = "10.0.0.0/16"
+# }
 
-resource "aws_subnet" "eks_subnet" {
-  count                   = 2
-  vpc_id                  = aws_vpc.eks_vpc.id
-  cidr_block              = "10.0.${count.index}.0/24"
-  availability_zone       = element(["us-east-1a", "us-east-1b"], count.index)
-  map_public_ip_on_launch = true
-}
+# resource "aws_subnet" "eks_subnet" {
+#   count                   = 2
+#   vpc_id                  = aws_vpc.eks_vpc.id
+#   cidr_block              = "10.0.${count.index}.0/24"
+#   availability_zone       = element(["us-east-1a", "us-east-1b"], count.index)
+#   map_public_ip_on_launch = true
+# }
 
 output "eks_cluster_name" {
-  value = aws_eks_cluster.techchallenge_eks_cluster.name
+  value = data.aws_vpc.techchallenge-vpc.name
 }
 
 output "fargate_profile_name" {
--- eks/main.tf
+++ eks/main.tf
@@ -1,5 +1,9 @@
 terraform {
-  backend "s3" {}
+    backend "s3" {
+    bucket = "techchallangebucket"
+    key    = "eks.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {}
