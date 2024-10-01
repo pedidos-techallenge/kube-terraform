f20811118d862ad247edab44818177918de2c22b
ederpbrito <ederpsilva@hotmail.com>
2024-10-01 19:35:47 -0300

    add infra

--- eks/eks.tf
+++ eks/eks.tf
@@ -1,86 +1,41 @@
 ## EKS subnets
 #Referencing public subnets
-data "aws_subnets" "public-subnets" {
+data "aws_vpc" "techchallenge-vpc" {
+  filter {
+    name   = "tag:Name"
+    values = ["techchallenge-vpc"]
+  }
+}
+
+data "aws_subnets" "private" {
+  filter {
+    name   = "tag:kubernetes.io/role/internal-elb"
+    values = ["1"]
+  }
+
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.techchallenge-vpc.id]
   }
-
-  filter {
-    name   = "tag:Name"
-    values = ["*public*"]
-  }
 }
 
-data "aws_subnet" "public_az1" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.techchallenge-vpc.id]
-  }
-  filter {
-    name   = "tag:Name"
-    values = ["subnet-public-az1"]
-  }
-  
-}
-
-data "aws_subnet" "public_az2" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.techchallenge-vpc.id]
-  }
-  filter {
-    name   = "tag:Name"
-    values = ["subnet-public-az2"]
-  }
-}
-
-data "aws_subnet" "private_az1" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.techchallenge-vpc.id]
-  }
-  filter {
-    name   = "tag:Name"
-    values = ["subnet-private-az1"]
-  }
-  
-}
-
-data "aws_subnet" "private_az2" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.techchallenge-vpc.id]
-  }
-  filter {
-    name   = "tag:Name"
-    values = ["subnet-private-az2"]
-  }
-}
-
-
-resource "aws_eks_cluster" "techchallenge_eks_cluster" {
+resource "aws_eks_cluster" "techchallenge_eks_cluster" {  
   name     = "techchallenge-eks-cluster"
   role_arn = "arn:aws:iam::117590171476:role/LabRole"
   version  = "1.30"
 
   vpc_config {
     security_group_ids = [aws_security_group.eks_security_group.id]
-    subnet_ids = [
-      data.aws_subnet.public_az1.id,
-      data.aws_subnet.public_az2.id,
-    ]
+    subnet_ids         = data.aws_subnets.private.ids        
   }
 }
 
-resource "aws_eks_fargate_profile" "fargate_profile" {
+
+resource "aws_eks_fargate_profile" "fargate_profile" {  
   cluster_name           = "techchallenge-eks-cluster"
   fargate_profile_name   = "fargate-profile"
   pod_execution_role_arn = "arn:aws:iam::117590171476:role/LabRole"
-  subnet_ids = [
-    data.aws_subnet.private_az1.id,
-    data.aws_subnet.private_az2.id,
-  ]
+  subnet_ids             = data.aws_subnets.private.ids    
 
   selector {
     namespace = "default"
@@ -93,13 +48,5 @@ resource "aws_eks_fargate_profile" "fargate_profile" {
 resource "aws_security_group" "eks_security_group" {
   name        = "eks-security-group"
   description = "Controla o acesso ao cluster EKS"
-  vpc_id      = data.aws_vpc.techchallenge-vpc.id
-}
-
-output "eks_cluster_name" {
-  value = data.aws_vpc.techchallenge-vpc.tags.Name
-}
-
-output "fargate_profile_name" {
-  value = aws_eks_fargate_profile.fargate_profile.fargate_profile_name
+  vpc_id      =  data.aws_vpc.techchallenge-vpc.id  
 }
--- eks/main.tf
+++ eks/main.tf
@@ -1,17 +1,6 @@
 terraform {
-    backend "s3" {
-    bucket = "techchallangebucket"
-    key    = "eks.tfstate"
-    region = "us-east-1"
+  backend "s3" { 
   }
 }
 
 provider "aws" {}
-
-### General data sources
-data "aws_vpc" "techchallenge-vpc" {
-  filter {
-    name   = "tag:Name"
-    values = ["techchallenge-vpc"]
-  }
-}
